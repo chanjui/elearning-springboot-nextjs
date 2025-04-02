46b15d74b1393bbb6769abd403269199b72d6333
heo chan hui <heochanhui6@gmail.com>
2025-04-02 10:10:33 +0900

    머지전

--- src/main/java/com/elearning/course/repository/CourseRatingRepository.java
+++ src/main/java/com/elearning/course/repository/CourseRatingRepository.java
@@ -1,11 +1,15 @@
 package com.elearning.course.repository;
 
 import com.elearning.course.entity.CourseRating;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface CourseRatingRepository extends JpaRepository<CourseRating, Long> {
-  @Query("SELECT AVG(cr.rating) FROM CourseRating cr WHERE cr.course.id = :courseId")
-  Double findAverageRatingByCourseId(@Param("courseId") Long courseId);
+  @Query("SELECT cr.course.id as courseId, AVG(cr.rating) as avgRating FROM CourseRating cr WHERE cr.course.id IN :courseIds GROUP BY cr.course.id")
+  List<Object[]> findAverageRatingsByCourseIds(@Param("courseIds") List<Long> courseIds);
+
 }

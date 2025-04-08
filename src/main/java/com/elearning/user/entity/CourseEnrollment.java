7558bdcfda82bb6a532a20ea2dd1def9fd14fcf5
heo chan hui <heochanhui6@gmail.com>
2025-04-08 16:16:55 +0900

    4.8/2차커밋

--- src/main/java/com/elearning/user/entity/CourseEnrollment.java
+++ src/main/java/com/elearning/user/entity/CourseEnrollment.java
@@ -1,17 +1,18 @@
 package com.elearning.user.entity;
 
+import com.elearning.common.entity.BaseEntity;
 import com.elearning.course.entity.Course;
 import jakarta.persistence.*;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Entity
-@Getter
-@NoArgsConstructor
 @Table(name = "courseEnrollment")
+@Getter
+@Setter
 public class CourseEnrollment {
 
     @Id
@@ -19,19 +20,22 @@ public class CourseEnrollment {
     private Long id;
     
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "userId")
+    @JoinColumn(name = "userId", nullable = false)
     private User user;
     
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "courseId")
+    @JoinColumn(name = "courseId", nullable = false)
     private Course course;
     
-    @Column(nullable = false)
+    @Column(name = "enrolledAt")
     private LocalDateTime enrolledAt = LocalDateTime.now();
     
-    @Column(nullable = false, precision = 5, scale = 2)
+    @Column(precision = 5, scale = 2)
     private BigDecimal progress = BigDecimal.ZERO;
     
     @Column(nullable = false, columnDefinition = "TINYINT(1) DEFAULT 0")
     private boolean completionStatus = false;
+
+    @Column(nullable = false, columnDefinition = "TINYINT(1) DEFAULT 0")
+    private boolean isDel = false;
 } 

6fdf9751a00c035a8e818f2d1e6c79547574afd6
Jungillwoo <zuirune@gmail.com>
2025-04-22 21:44:10 +0900

    webSocket ip ec2 퍼블릭ip주소로 변경

--- lib/socket.ts
+++ lib/socket.ts
@@ -8,7 +8,7 @@ export const connectSocket = (
   onConnect?: () => void
 ) => {
   stompClient = new Client({
-    webSocketFactory: () => new SockJS("http://192.168.0.7:8080/ws"), // SockJS 사용
+    webSocketFactory: () => new SockJS("http://3.34.90.186/ws"), // SockJS 사용
     // webSocketFactory: () => new SockJS("http://localhost:8080/ws"),
     reconnectDelay: 5000,
     onConnect: () => {

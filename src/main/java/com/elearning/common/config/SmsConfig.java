92895aaa8c6d061617355a56bfac549049f7e152
heo chan hui <heochanhui6@gmail.com>
2025-04-22 15:47:30 +0900

    sm

--- src/main/java/com/elearning/common/config/SmsConfig.java
+++ src/main/java/com/elearning/common/config/SmsConfig.java
@@ -5,9 +5,11 @@ import net.nurigo.java_sdk.api.Message;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 @RequiredArgsConstructor
+@Profile("sms") // SMS 기능이 필요한 프로필에서만 로드
 public class SmsConfig {
 
   @Value("${coolsms.api-key}")

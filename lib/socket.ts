89fed3a119784d05132a2d1cd0eab7cba4e755f3
Jungillwoo <zuirune@gmail.com>
2025-04-22 21:41:43 +0900

    채팅 메시지 읽음처리 수정중

--- lib/socket.ts
+++ lib/socket.ts
@@ -8,7 +8,7 @@ export const connectSocket = (
   onConnect?: () => void
 ) => {
   stompClient = new Client({
-    webSocketFactory: () => new SockJS("http://192.168.0.7:8080/ws"), // SockJS 사용
+    webSocketFactory: () => new SockJS("http://192.168.35.75:8080/ws"), // SockJS 사용
     // webSocketFactory: () => new SockJS("http://localhost:8080/ws"),
     reconnectDelay: 5000,
     onConnect: () => {

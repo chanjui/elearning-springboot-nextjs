05bb8113a4e1b535284c2bfaf76cfab241f5a767
heo chan hui <heochanhui6@gmail.com>
2025-04-23 16:57:10 +0900

    ㄴ

--- src/main/java/com/elearning/common/config/WebSocketConfig.java
+++ src/main/java/com/elearning/common/config/WebSocketConfig.java
@@ -10,15 +10,14 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
-    // 클라이언트가 연결할 수 있는 WebSocket 엔드포인트 지정
-    registry.addEndpoint("/ws")  // 이게 필요함!
-      .setAllowedOriginPatterns("*")  // CORS 허용
-      .withSockJS(); // SockJS 사용 시
+    registry.addEndpoint("/ws")
+      .setAllowedOriginPatterns("https://elearning-frontend-smoky.vercel.app", "http://localhost:3000")
+      .withSockJS();
   }
 
   @Override
   public void configureMessageBroker(MessageBrokerRegistry registry) {
-    registry.enableSimpleBroker("/topic"); // 프론트 구독 경로: "/topic/chat"
-    registry.setApplicationDestinationPrefixes("/app"); // 프론트 전송 경로: "/app/chat/message"
+    registry.enableSimpleBroker("/topic");
+    registry.setApplicationDestinationPrefixes("/app");
   }
 }

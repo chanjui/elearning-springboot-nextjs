54b614977145a272e44409f1f718698a43960a3e
heo chan hui <heochanhui6@gmail.com>
2025-04-28 14:22:44 +0900

    일우머지

--- components/chat/floating-contact-button-wrapper.tsx
+++ components/chat/floating-contact-button-wrapper.tsx
@@ -2,7 +2,7 @@
 
 
 import useUserStore from "@/app/auth/userStore"
-import { FloatingContactButton } from "./new-massage-modal"
+import { FloatingContactButton } from '@/components/chat/new-massage-modal';
 
 export function FloatingContactButtonWrapper() {
   const { user } = useUserStore()

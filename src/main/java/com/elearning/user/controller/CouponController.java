452a052e2f81ac1176c9628716af5eb3a87df63c
heo chan hui <heochanhui6@gmail.com>
2025-05-21 17:33:54 +0900

    3탄

--- src/main/java/com/elearning/user/controller/CouponController.java
+++ src/main/java/com/elearning/user/controller/CouponController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/user/coupons")
+@RequestMapping("/api/mypage/coupons")
 @RequiredArgsConstructor
 public class CouponController {
 
--- src/main/java/com/elearning/user/controller/MyLearningDashboardController.java
+++ src/main/java/com/elearning/user/controller/MyLearningDashboardController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/user/dashboard")
+@RequestMapping("/api/mypage/dashboard")
 @RequiredArgsConstructor
 public class MyLearningDashboardController {
 

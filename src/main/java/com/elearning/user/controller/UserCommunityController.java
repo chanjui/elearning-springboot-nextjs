cfb0d2dedf211453eff9470963f6063fa0996695
heo chan hui <heochanhui6@gmail.com>
2025-05-21 18:07:30 +0900

    내글,내댓글 카운트

--- src/main/java/com/elearning/user/controller/UserCommunityController.java
+++ src/main/java/com/elearning/user/controller/UserCommunityController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/user/community")
+@RequestMapping("/api/mypage/community")
 @RequiredArgsConstructor
 public class UserCommunityController {
 

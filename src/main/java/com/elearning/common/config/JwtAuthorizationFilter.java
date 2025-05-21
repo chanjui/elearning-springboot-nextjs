9d34c14e8381f810b41a30d3ec87b6f88f5a6458
heo chan hui <heochanhui6@gmail.com>
2025-05-21 15:38:00 +0900

    강의안나오는거수정

--- src/main/java/com/elearning/common/config/JwtAuthorizationFilter.java
+++ src/main/java/com/elearning/common/config/JwtAuthorizationFilter.java
@@ -27,7 +27,9 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
   private final AntPathMatcher antPathMatcher = new AntPathMatcher();
 
   private boolean isPublicPath(String path) {
-    return (antPathMatcher.match("/api/user/**", path) && !antPathMatcher.match("/api/user/coupons", path)) ||
+    return (antPathMatcher.match("/api/user/**", path) && 
+           !antPathMatcher.match("/api/user/coupons", path) && 
+           !antPathMatcher.match("/api/user/courses", path)) ||
            antPathMatcher.match("/api/course/**", path) ||
            antPathMatcher.match("/api/auth/**", path) ||
            antPathMatcher.match("/api/categories/**", path) ||
